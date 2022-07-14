9fb548a00284eac6ccc1ecd6d5ef9f8dde6f1ed9
Diana Huang <dianahuang@Dianas-MBP.lan>
2022-07-14 08:46:18 -1000

    created_at

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+import datetime
+
 db = SQLAlchemy()
 
 
@@ -8,7 +10,6 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
-    
 
 class User(db.Model):
     """User"""
@@ -23,3 +24,23 @@ class User(db.Model):
     last_name = db.Column(db.Text,
                      nullable=False)
     image_url = db.Column(db.Text, nullable=False, default="https://thevoicefinder.com/wp-content/themes/the-voice-finder/images/default-img.png")
+                          
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+
+# class Post(db.Model):
+#     """Post"""
+
+#     __tablename__ = "Post"
+
+#     id = db.Column(db.Integer,
+#                    primary_key=True,
+#                    autoincrement=True)
+#     title = db.Column(db.Text,
+#                      nullable=False)
+#     context = db.Column(db.Text,
+#                      nullable=False)
+#     created_at = db.Column(db.Text, nullable=False)
+#     user_id = db.Column(
+#         db.Text,
+#         db.ForeignKey('User.id'))
+#     user = db.relationship('User', backref='posts')
